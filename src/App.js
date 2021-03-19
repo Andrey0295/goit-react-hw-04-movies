26fe5cb1814a00361fd5eafe492d1ee908086dc0
Andrey0295 <andreevicandrej00@gmail.com>
2021-03-19 17:43:01 +0200

    реализует ленивую загрузку для маршрутов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import AppBar from './Components/AppBar/AppBar';
 
-import HomePageView from './views/HomePageView';
-import MovieDetailsPageView from './views/MovieDetailsPageView';
-import MoviesPageView from './views/MoviesPageView';
+// import HomePageView from './views/HomePageView';
+// import MovieDetailsPageView from './views/MovieDetailsPageView';
+// import MoviesPageView from './views/MoviesPageView';
 import NotFoundView from './views/NotFoundView';
 import routes from '../src/routes';
+
+const HomePageView = lazy(() =>
+  import('./views/HomePageView' /* webpackChunkName: "home-view" */),
+);
+const MoviesPageView = lazy(() =>
+  import('./views/MoviesPageView' /* webpackChunkName: "movies-view" */),
+);
+const MovieDetailsPageView = lazy(() =>
+  import(
+    './views/MovieDetailsPageView' /* webpackChunkName: "movies-details-view" */
+  ),
+);
 
 const App = () => {
   return (
     <>
       <AppBar />
-      <Switch>
-        <Route exact path={routes.home} component={HomePageView} />
-        <Route exact path={routes.movies} component={MoviesPageView} />
-        <Route path={routes.movieDetails} component={MovieDetailsPageView} />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Switch>
+          <Route exact path={routes.home} component={HomePageView} />
+          <Route exact path={routes.movies} component={MoviesPageView} />
+          <Route path={routes.movieDetails} component={MovieDetailsPageView} />
 
-        <Route component={NotFoundView} />
-      </Switch>
+          <Route component={NotFoundView} />
+        </Switch>
+      </Suspense>
     </>
   );
 };
--- src/views/HomePageView.js
+++ src/views/HomePageView.js
@@ -6,6 +6,7 @@ import MoviesList from '../Components/MoviesList/MoviesList';
 class HomePageView extends Component {
   state = {
     movies: [],
+    error: null,
   };
 
   async componentDidMount() {
@@ -13,16 +14,23 @@ class HomePageView extends Component {
   }
 
   async fetchPopularMovies() {
-    const responce = await axios.get(
-      'https://api.themoviedb.org/3/trending/movie/day?api_key=ec0633f4801b6d57348783906eedf2d2',
-    );
+    const responce = await axios
+      .get(
+        'https://api.themoviedb.org/3/trending/movie/day?api_key=ec0633f4801b6d57348783906eedf2d2',
+      )
+      .catch(error => this.setState({ error }));
 
     this.setState({ movies: responce.data.results });
   }
 
   render() {
     const { movies } = this.state;
-    return <div>{movies.length > 0 && <MoviesList movies={movies} />}</div>;
+    return (
+      <div>
+        {movies.length > 0 && <MoviesList movies={movies} />}
+        {this.state.error && <h1>Error!!!</h1>}
+      </div>
+    );
   }
 }
 
